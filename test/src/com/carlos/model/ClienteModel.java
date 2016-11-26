f659af555a1c585ec14e6ff7e2d4349518506401
Carlos <Carlos@192.168.1.33>
2016-11-25 22:01:04 -0500

    fdfdf

---
+++ test/src/com/carlos/model/ClienteModel.java
@@ -0,0 +1,40 @@
+package com.carlos.model;
+
+import java.util.ArrayList;
+
+import com.carlos.pojo.Cliente;
+import com.carlos.repositories.IRepositoryBase;
+
+public class ClienteModel implements IRepositoryBase<Cliente> {
+
+	@Override
+	public boolean Add(Cliente t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Update(Cliente t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Remove(Cliente t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Cliente> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- test/src/com/carlos/repositories/IRepositoryBase.java
+++ test/src/com/carlos/repositories/IRepositoryBase.java
@@ -1,8 +1,12 @@
 package com.carlos.repositories;
 
+import java.util.ArrayList;
+
 public interface IRepositoryBase<TEntity> {
 	
 	boolean Add(TEntity t);
 	boolean Update(TEntity t);
 	boolean Remove(TEntity t);
+	ArrayList<TEntity> GetAll();
+	TEntity GetById(int id);
  }
