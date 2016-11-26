af9c39c47451dfbe965e96a48113605cbf97d3a0
jcarlosCode <ritchie.hiroshi0306@gmail.com>
2016-11-25 21:54:32 -0500

    fffffffffffffffffff
    ffffffffff

--- test/src/com/carlos/repositories/IRepositoryBase.java
+++ test/src/com/carlos/repositories/IRepositoryBase.java
@@ -2,4 +2,8 @@ package com.carlos.repositories;
 
 public interface IRepositoryBase {
   //hola soy un comentario
+  //hola soy un comentario
+  //hola soy un comentario
+  //hola soy un comentario
+  //hola soy un comentario
 }
